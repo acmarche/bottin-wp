b0b3390b3de70a557708a54b0262c7abf4385fc5
jfsenechal <jfsenechal@gmail.com>
2022-08-08 16:47:23 +0200

    fix env load

--- src/Env.php
+++ src/Env.php
@@ -2,21 +2,12 @@
 
 namespace AcMarche\Bottin;
 
-use AcMarche\Pivot\DependencyInjection\Kernel;
-use Exception;
-use Symfony\Component\Dotenv\Dotenv;
+use AcMarche\Pivot\DependencyInjection\PivotContainer;
 
 class Env
 {
     public static function loadEnv(): void
     {
-        $dir = Kernel::getDir();
-        $dotenv = new Dotenv();
-        try {
-            // loads .env, .env.local, and .env.$APP_ENV.local or .env.$APP_ENV
-            $dotenv->load($dir . '.env');
-        } catch (Exception $exception) {
-            echo "error load env: " . $exception->getMessage();
-        }
+        PivotContainer::init();
     }
 }
