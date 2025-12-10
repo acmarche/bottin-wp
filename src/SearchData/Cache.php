13c73e72e2aabafc98e4f7874ffc161e0a391943
jfsenechal <jfsenechal@gmail.com>
2025-12-10 10:31:49 +0100

    up

--- src/SearchData/Cache.php
+++ src/SearchData/Cache.php
@@ -4,8 +4,8 @@ namespace AcMarche\Bottin\SearchData;
 
 use Psr\Cache\CacheItemPoolInterface;
 use Psr\Cache\InvalidArgumentException;
-use Symfony\Component\Cache\Adapter\ApcuAdapter;
-use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use Symfony\Component\Cache\Adapter\RedisAdapter;
+use Symfony\Component\Cache\Adapter\RedisTagAwareAdapter;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\String\Slugger\AsciiSlugger;
 use Symfony\Component\String\Slugger\SluggerInterface;
@@ -16,48 +16,16 @@ class Cache
 {
     public static $instanceObject = null;
     private static ?SluggerInterface $slugger = null;
+    private static ?CacheInterface $cache = null;
 
-    public static function instance(): CacheInterface
+    public static function instance(): CacheInterface|RedisTagAwareAdapter
     {
-        if (self::$instanceObject) {
-            return self::$instanceObject;
+        if (!self::$cache) {
+            $client = RedisAdapter::createConnection('redis://localhost');
+            self::$cache = new RedisTagAwareAdapter($client, 'marchebe', 60 * 60 * 8);
         }
 
-        if (extension_loaded('apc') && ini_get('apc.enabled')) {
-            self::$instanceObject =
-                new ApcuAdapter(
-                // a string prefixed to the keys of the items stored in this cache
-                    $namespace = 'newmarche',
-
-                    // the default lifetime (in seconds) for cache items that do not define their
-                    // own lifetime, with a value 0 causing items to be stored indefinitely (i.e.
-                    // until the APCu memory is cleared)
-                    $defaultLifetime = 3600,
-
-                    // when set, all keys prefixed by $namespace can be invalidated by changing
-                    // this $version string
-                    $version = null
-                );
-        } else {
-            self::$instanceObject =
-                new FilesystemAdapter(
-                // a string used as the subdirectory of the root cache directory, where cache
-                // items will be stored
-                    $namespace = 'newmarche2',
-
-                    // the default lifetime (in seconds) for cache items that do not define their
-                    // own lifetime, with a value 0 causing items to be stored indefinitely (i.e.
-                    // until the files are deleted)
-                    $defaultLifetime = 3600,
-
-                    // the main cache directory (the application needs read-write permissions on it)
-                    // if none is specified, a directory is created inside the system temporary directory
-                    $directory = null
-                );
-        }
-
-        //return new TagAwareAdapter(self::$instanceObject, self::$instanceObject);
-        return self::$instanceObject;
+        return self::$cache;
     }
 
     public static function refresh(string $code): void
@@ -73,18 +41,19 @@ class Cache
 
     public static function generateCodeBottin(int $blogId, string $slug): string
     {
-        return 'bottin-fiche-'.$blogId.'-'.$slug;
+        return 'bottin-fiche-' . $blogId . '-' . $slug;
     }
 
     public static function generateCodeArticle(int $blogId, int $postId): string
     {
-        return 'post-'.$blogId.'-'.$postId;
+        return 'post-' . $blogId . '-' . $postId;
     }
 
     public static function generateCodeCategory(int $blogId, int $categoryId): string
     {
-        return 'category-'.$blogId.'-'.$categoryId;
+        return 'category-' . $blogId . '-' . $categoryId;
     }
+
     public static function generateKey(string $cacheKey): string
     {
         if (!self::$slugger) {
@@ -95,6 +64,7 @@ class Cache
 
         return self::$slugger->slug($keyUnicode->ascii()->toString());
     }
+
     // Helper method to delete an item from cache
     public static function delete(string $key): bool
     {
@@ -121,7 +91,7 @@ class Cache
             try {
                 $item = $cache->getItem($cacheKey);
             } catch (InvalidArgumentException $e) {
-                 return null;
+                return null;
             }
             return $item->isHit() ? $item->get() : null;
         }
